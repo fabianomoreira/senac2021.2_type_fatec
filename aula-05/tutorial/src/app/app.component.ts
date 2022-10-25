baddb96e73b84c151654713c543d75f542116133
Fabiano <professor@fabianomoreira.com.br>
2022-10-25 09:48:12 -0300

    Implementações da aula 06 e material

--- aula-05/tutorial/src/app/app.component.ts
+++ aula-05/tutorial/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tutorial';
+  title = 'Aula 06 de Typescript';
+  name = 'Flamengo';
 }
